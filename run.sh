151e08d09663902ad2d526d80786eea68dab5c8d
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2018-02-26 18:09:11 +0100

    generator

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sed -i.bak 's/'%mailpass%'/'"$MAILPASSWORD"'/' /var/www/.env
+sed -i.bak 's/'%dbpass%'/'"$PASSWORD"'/' /var/www/.env
+
 cd /var/www
 php yii generation
 php yii reminder
